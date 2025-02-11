7e230be8ff4013eaa7430add52cc0632191b28f3
Alexander <fortuna12_12@mail.ru>
2025-02-11 20:20:43 +0700

    Added an abstract model for convenience

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
+
 from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=False)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, )
 DATABASE_URL = settings.DATABASE_URL
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+    pass

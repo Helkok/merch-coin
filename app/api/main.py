3fdf79d49c6367bdafbeac768392a9293e4a71e0
Alexander <fortuna12_12@mail.ru>
2025-02-13 00:13:05 +0700

    File to connect all routers, now only auth_router

---
+++ app/api/main.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from app.api.auth import router as auth_router
+from app.constants import RESPONSES
+
+api_router = APIRouter()
+
+api_router.include_router(auth_router, tags=["auth"], responses=RESPONSES)

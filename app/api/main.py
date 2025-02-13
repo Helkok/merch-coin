4153e6abec5b05bb5fc1f83dfd0c2df7c60178c6
Alexander <fortuna12_12@mail.ru>
2025-02-14 03:27:58 +0700

    Include transaction_router and user_router

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.auth import router as auth_router
+from app.api.routers.auth import router as auth_router
+from app.api.routers.transactions import router as transaction_router
+from app.api.routers.user_info import router as user_router
 from app.utils.constants import RESPONSES
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, tags=["auth"], responses=RESPONSES)
+api_router.include_router(transaction_router, tags=["transactions"], responses=RESPONSES)
+api_router.include_router(user_router, tags=["users"], responses=RESPONSES)
